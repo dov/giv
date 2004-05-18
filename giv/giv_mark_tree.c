8ed248aa95131be42e580afc951d9de27e3935cd
Dov Grobgeld <dov@src.gnome.org>
2004-05-18 15:01:41 +0000

    Updated my new email address.

--- giv/giv_mark_tree.c
+++ giv/giv_mark_tree.c
@@ -1,5 +1,5 @@
 /* 
- * Copyright (c) 2000 by Dov Grobgeld <dov@imagic.weizmann.ac.il>
+ * Copyright (c) 2000 by Dov Grobgeld <dov.grobgeld@weizmann.ac.il>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
