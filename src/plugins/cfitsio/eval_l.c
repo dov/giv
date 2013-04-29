9c8a2a0540ea4eb35b3b83e60d3da3ef72e696e8
Dov Grobgeld <dov.grobgeld@gmail.com>
2013-04-29 23:19:22 +0300

    Fixed cfitsio compilation under Windows. Use system library under Linux.

--- src/plugins/cfitsio/eval_l.c
+++ src/plugins/cfitsio/eval_l.c
@@ -2212,41 +2212,5 @@ static int find_variable(char *varName)
 
 #if defined(vms) || defined(__vms) || defined(WIN32) || defined(__WIN32__) || defined(macintosh)
 
-/* ================================================================== */
-/* A hack for nonunix machines, which lack strcasecmp and strncasecmp */
-/* ================================================================== */
-
-int strcasecmp(const char *s1, const char *s2)
-{
-   char c1, c2;
-
-   for (;;) {
-      c1 = toupper( *s1 );
-      c2 = toupper( *s2 );
-
-      if (c1 < c2) return(-1);
-      if (c1 > c2) return(1);
-      if (c1 == 0) return(0);
-      s1++;
-      s2++;
-   }
-}
-
-int strncasecmp(const char *s1, const char *s2, size_t n)
-{
-   char c1, c2;
-
-   for (; n-- ;) {
-      c1 = toupper( *s1 );
-      c2 = toupper( *s2 );
-
-      if (c1 < c2) return(-1);
-      if (c1 > c2) return(1);
-      if (c1 == 0) return(0);
-      s1++;
-      s2++;
-   }
-   return(0);
-}
 
 #endif
